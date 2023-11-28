1eeff4fad82998d9c793e347ad94469d4d3979fe
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2023-11-29 03:01:14 +0500

    Added Login API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const registerUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
@@ -31,7 +32,28 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-    
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are required");
+    }
+    const user = await User.findOne({email});
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id,
+                },
+            }, process.env.ACCESS_TOKEN_SECRET,
+            {expiresIn: "365d"}
+        );
+        res.status(200).json({accessToken});
+    }
+    else{
+        res.status(401);
+        throw new Error("Email or password is not valid.");
+    }
 });
 
 const currentUser = asyncHandler(async (req, res) => {
